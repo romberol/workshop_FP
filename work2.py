ac5783454abf4bc8a539a41ee1faabe1d138c8ba
romberol <93192972+romberol@users.noreply.github.com>
2021-12-15 12:27:30 +0200

    Added functions

--- work2.py
+++ work2.py
@@ -6,15 +6,12 @@ def readfile():
     except FileNotFoundError:
         data = pd.DataFrame(columns=["Date", "Priority", "Task", "State"], data=[[None, None, None, None]])
         data.to_csv("deadline.csv", encoding='utf-8', index=False)
-    print("Your deadline table:")
-    if data.dropna().shape==(0, 4):
-        print("Your tabel is empty!")
-    else:
-        print(data)
     return data
 
 def check_done(table):
     pass
+
+
 def add_deadlines(table):
     newdata_ls = []
     question = input('Do you want to add something? (type anything if so, type 0 if no)')
@@ -28,12 +25,12 @@ press enter twice, if that is all')
     while True:
         new_datum = input()
         if new_datum == '':
-            print('The data entered was:', newdata_ls)
             break
-        new_datum  = new_datum.split()
+        new_datum  = new_datum.split(" ")
         newdata_ls.append(new_datum)
     newdata_ls.sort(key=lambda x: ''.join(x[0].split(':'))[::-1])
-    print('list sorted by data', newdata_ls)
-    return newdata_ls
+    return pd.DataFrame(data = newdata_ls, columns=table.columns)
+
+
 def check_input():
     pass
