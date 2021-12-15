c035eb7fa6598aabf27242a1347108b5fe99c791
romberol <93192972+romberol@users.noreply.github.com>
2021-12-15 11:57:32 +0200

    Added readfile()

--- work2.py
+++ work2.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
-def readfile(path):
-    pass
+def readfile():
+    try:
+        data = pd.read_csv("deadline.csv")
+    except FileNotFoundError:
+        data = pd.DataFrame(columns=["Date", "Priority", "Task", "State"], data=[[None, None, None, None]])
+        data.to_csv("deadline.csv", encoding='utf-8', index=False)
+    print("Your deadline table:")
+    if data.dropna().shape==(0, 4):
+        print("Your tabel is empty!")
+    else:
+        print(data)
+    return data
+
 def check_done(table):
     pass
 def add_deadlines(table):
