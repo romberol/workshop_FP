bc623800ed9eba41e1d9acb9fde6fb96dfe3621d
Coder121314 <87498412+Coder121314@users.noreply.github.com>
2021-12-15 12:12:28 +0200

    added add_deadlines

--- work2.py
+++ work2.py
@@ -16,6 +16,24 @@ def readfile():
 def check_done(table):
     pass
 def add_deadlines(table):
-    pass
+    newdata_ls = []
+    question = input('Do you want to add something? (type anything if so, type 0 if no)')
+    if question == '0':
+        print('No changes are made')
+        return False
+    print('What exactly do you want to add?\
+Please, enter datum in format of: date priority task status;\
+press enter to enter next datum;\
+press enter twice, if that is all')
+    while True:
+        new_datum = input()
+        if new_datum == '':
+            print('The data entered was:', newdata_ls)
+            break
+        new_datum  = new_datum.split()
+        newdata_ls.append(new_datum)
+    newdata_ls.sort(key=lambda x: ''.join(x[0].split(':'))[::-1])
+    print('list sorted by data', newdata_ls)
+    return newdata_ls
 def check_input():
     pass
